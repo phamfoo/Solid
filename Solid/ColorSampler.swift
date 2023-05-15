95adefb487165387190e1b14c4824c178b2b397f
Tien Pham <tien@pham.foo>
2023-05-15 10:23:36 +0700

    Update color sampler button

--- Solid/ColorSampler.swift
+++ Solid/ColorSampler.swift
@@ -1,12 +1,18 @@
 import SwiftUI
 
 struct ColorSampler: View {
+    @State private var hovered = false
+    @State private var sampling = false
+
     var onPick: (NSColor) -> Void
 
     var body: some View {
         Button {
+            sampling = true
             NSColorSampler()
                 .show { pickedColor in
+                    sampling = false
+
                     if let pickedColor {
                         self.onPick(pickedColor)
                     }
@@ -14,8 +20,22 @@ struct ColorSampler: View {
         } label: {
             Image(systemName: "eyedropper")
                 .imageScale(.large)
+                .padding(8)
+                .background {
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(.quaternary)
+                        .opacity(hovered ? 1 : 0)
+                }
+                .background {
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(Color.accentColor)
+                        .opacity(sampling ? 1 : 0)
+                }
         }
         .buttonStyle(.plain)
+        .onHover { hovered in
+            self.hovered = hovered
+        }
     }
 }
 
