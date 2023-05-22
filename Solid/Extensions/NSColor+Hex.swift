d8e976dffc0aa791768f9ae40180d953807aeb21
Tien Pham <tien@pham.foo>
2023-05-22 20:50:23 +0700

    Include alpha component in the hex string

--- Solid/Extensions/NSColor+Hex.swift
+++ Solid/Extensions/NSColor+Hex.swift
@@ -5,8 +5,13 @@ extension NSColor {
         let red = Int((redComponent * 255).rounded())
         let green = Int((greenComponent * 255).rounded())
         let blue = Int((blueComponent * 255).rounded())
+        let alpha = Int((alphaComponent * 255).rounded())
 
-        return String(format: "#%02x%02x%02x", red, green, blue)
+        if alpha == 255 {
+            return String(format: "#%02x%02x%02x", red, green, blue)
+        } else {
+            return String(format: "#%02x%02x%02x%02x", red, green, blue, alpha)
+        }
     }
 }
 
