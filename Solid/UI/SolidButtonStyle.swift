2725dbf0bf361f272c527d03633e9fd49b61930f
Tien Pham <tien@pham.foo>
2023-05-22 13:49:44 +0700

    Add scale effect to button style

--- Solid/UI/SolidButtonStyle.swift
+++ Solid/UI/SolidButtonStyle.swift
@@ -13,10 +13,15 @@ struct SolidButtonStyle: ButtonStyle {
                     .fill(.quaternary)
                     .opacity(hovered ? 1 : 0)
             }
+            .scaleEffect(configuration.isPressed ? 0.95 : 1)
             .onHover { hovered in
                 self.hovered = hovered
             }
             .animation(.easeInOut(duration: 0.1), value: hovered)
+            .animation(
+                .easeInOut(duration: 0.1),
+                value: configuration.isPressed
+            )
     }
 }
 
