d6a66ecf28e145c875010b54c51d3c7f1cdd4350
Tien Pham <tien@pham.foo>
2023-05-23 19:49:40 +0700

    Implement stay on top

--- Solid/SolidApp.swift
+++ Solid/SolidApp.swift
@@ -1,7 +1,10 @@
+import Defaults
 import SwiftUI
 
 @main
 struct SolidApp: App {
+    @Default(.stayOnTop) private var stayOnTop
+
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
@@ -11,6 +14,29 @@ struct SolidApp: App {
                     \.managedObjectContext,
                     persistenceController.container.viewContext
                 )
+                .onChange(of: stayOnTop) { stayOnTop in
+                    for window in NSApplication.shared.windows {
+                        if stayOnTop {
+                            window.level = .floating
+                        } else {
+                            window.level = .normal
+                        }
+                    }
+                }
+                .onReceive(
+                    NotificationCenter.default
+                        .publisher(
+                            for: NSWindow.didBecomeKeyNotification
+                        )
+                ) { _ in
+                    for window in NSApplication.shared.windows {
+                        if stayOnTop {
+                            window.level = .floating
+                        } else {
+                            window.level = .normal
+                        }
+                    }
+                }
         }
         .windowToolbarStyle(.unified(showsTitle: false))
 
