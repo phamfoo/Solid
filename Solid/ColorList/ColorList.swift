945d0c518a1037014eed770b5667e5bf209ffdb1
Tien Pham <tien@pham.foo>
2023-05-29 17:32:55 +0700

    Improve search

--- Solid/ColorList/ColorList.swift
+++ Solid/ColorList/ColorList.swift
@@ -15,11 +15,39 @@ struct ColorList: View {
             searchText
         } set: { newValue in
             searchText = newValue
+            var predicates: [NSPredicate] = []
+
+            if !newValue.isEmpty {
+                predicates.append(
+                    NSPredicate(format: "name CONTAINS[c] %@", newValue)
+                )
+
+                let colorPredicates: [String: NSPredicate] = [
+                    "black": NSPredicate(format: "brightness < 0.1"),
+                    "blue": NSPredicate(format: "hue >= 0.52 AND hue <= 0.67 AND saturation >= 0.4 AND brightness >= 0.4"),
+                    "brown": NSPredicate(format: "((hue >= 0 AND hue <= 0.08) OR (hue >= 0.92 AND hue <= 1)) AND saturation >= 0.25 AND brightness >= 0.1 AND brightness <= 0.7"),
+                    "gray": NSPredicate(format: "saturation < 0.05 AND brightness >= 0.05 AND brightness <= 0.95"),
+                    "green": NSPredicate(format: "hue >= 0.2 AND hue <= 0.4 AND saturation >= 0.3 AND brightness >= 0.3"),
+                    "orange": NSPredicate(format: "hue >= 0.05 AND hue <= 0.12 AND saturation >= 0.5 AND brightness >= 0.5"),
+                    "pink": NSPredicate(format: "hue >= 0.85 AND saturation >= 0.3 AND brightness >= 0.7"),
+                    "purple": NSPredicate(format: "hue >= 0.75 AND hue <= 0.95 AND saturation >= 0.3 AND brightness >= 0.5"),
+                    "red": NSPredicate(format: "hue >= 0.95 OR hue <= 0.05 AND saturation >= 0.5 AND brightness >= 0.5"),
+                    "white": NSPredicate(format: "brightness >= 0.95"),
+                ]
+
+                if let predicate = colorPredicates[newValue] {
+                    predicates.append(
+                        predicate
+                    )
+                }
+            }
 
             sections.nsPredicate =
-                newValue.isEmpty
+                predicates.isEmpty
                     ? nil
-                    : NSPredicate(format: "name CONTAINS[c] %@", newValue)
+                    : NSCompoundPredicate(
+                        orPredicateWithSubpredicates: predicates
+                    )
         }
     }
 
