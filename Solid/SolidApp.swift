25c8356a540c37dad621f3c4d744942ce3f9bb5b
Tien Pham <tien@pham.foo>
2023-06-05 14:37:44 +0700

    Fix window frame not being restored properly

--- Solid/SolidApp.swift
+++ Solid/SolidApp.swift
@@ -14,7 +14,7 @@ struct SolidApp: App {
     @State private var menuBarController = MenuBarController()
 
     var body: some Scene {
-        WindowGroup {
+        WindowGroup(id: "main") {
             AppMenuBar(menuBarController: menuBarController) {
                 ContentView()
             }
@@ -37,31 +37,7 @@ struct SolidApp: App {
             ) { _ in
                 updateWindowLevel(stayOnTop: stayOnTop)
             }
-            // Save and restore window frame manually
-            .onReceive(
-                NotificationCenter.default
-                    .publisher(
-                        for: NSWindow.willCloseNotification
-                    )
-            ) { notification in
-                if let window = notification.object as? NSWindow,
-                   window == NSApplication.shared.mainWindow
-                {
-                    Defaults[.mainWindowFrame] = window.frame
-                }
-            }
-            .onReceive(
-                NotificationCenter.default
-                    .publisher(
-                        for: NSWindow.didBecomeKeyNotification
-                    )
-            ) { notification in
-                if let window = notification.object as? NSWindow,
-                   let savedWindowFrame = Defaults[.mainWindowFrame]
-                {
-                    window.setFrame(savedWindowFrame, display: true)
-                }
-            }
+            .modifier(RestoreWindowFrameModifier(windowGroupID: "main"))
         }
         .windowToolbarStyle(.unified(showsTitle: false))
         .commands {
@@ -83,6 +59,48 @@ struct SolidApp: App {
                 .filter { $0.level == .floating }
                 .forEach { $0.level = .normal }
         }
+    }
+}
+
+private struct RestoreWindowFrameModifier: ViewModifier {
+    @State private var shouldUpdateWindowFrame = true
+
+    var windowGroupID: String
+
+    func body(content: Content) -> some View {
+        content
+            // Save and restore window frame manually
+            .onReceive(
+                NotificationCenter.default
+                    .publisher(
+                        for: NSWindow.willCloseNotification
+                    )
+            ) { notification in
+                if let window = notification.object as? NSWindow,
+                   let windowID = window.identifier?.rawValue,
+                   windowID.starts(with: windowGroupID)
+                {
+                    Defaults[.mainWindowFrame] = window.frame
+                    shouldUpdateWindowFrame = true
+                }
+            }
+            .onReceive(
+                NotificationCenter.default
+                    .publisher(
+                        for: NSWindow.didBecomeMainNotification
+                    )
+            ) { notification in
+                if let window = notification.object as? NSWindow,
+                   let windowID = window.identifier?.rawValue,
+                   windowID.starts(with: "main"),
+                   let savedWindowFrame = Defaults[.mainWindowFrame]
+                {
+                    if shouldUpdateWindowFrame {
+                        window.setFrame(savedWindowFrame, display: true)
+                        shouldUpdateWindowFrame = false
+                    }
+                }
+            }
     }
 }
 
